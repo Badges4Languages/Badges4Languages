7b3ecb02bce87cb41c3831296a4a213e5ca4dc75
Antonio Devís <colomet@hotmail.com>
2016-07-24 18:45:59 +0200

    Update class-badges4languages-plugin-loader.php

--- includes/class-badges4languages-plugin-loader.php
+++ includes/class-badges4languages-plugin-loader.php
@@ -3,7 +3,7 @@
 /**
  * Register all actions and filters for the plugin
  *
- * @link       http://www.badges4languages.org
+ * @link       http://www.badges4languages.com
  * @since      1.0.0
  *
  * @package    Badges4languages_Plugin
