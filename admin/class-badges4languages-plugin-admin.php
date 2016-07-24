a1809093f036b70a4b4dc5e1304500ca057f68a4
Antonio Devís <colomet@hotmail.com>
2016-07-24 18:44:31 +0200

    Update class-badges4languages-plugin-admin.php

--- admin/class-badges4languages-plugin-admin.php
+++ admin/class-badges4languages-plugin-admin.php
@@ -3,7 +3,7 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       http://www.badges4languages.org
+ * @link       http://www.badges4languages.com
  * @since      1.0.0
  *
  * @package    Badges4languages_Plugin
