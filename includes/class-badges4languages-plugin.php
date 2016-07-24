2fa9d592c9010f9788772d65767adf9c08be95ea
Antonio Devís <colomet@hotmail.com>
2016-07-24 18:45:43 +0200

    Update class-badges4languages-plugin.php

--- includes/class-badges4languages-plugin.php
+++ includes/class-badges4languages-plugin.php
@@ -6,7 +6,7 @@
  * A class definition that includes attributes and functions used across both the
  * public-facing side of the site and the admin area.
  *
- * @link       http://www.badges4languages.org
+ * @link       http://www.badges4languages.com
  * @since      1.0.0
  *
  * @package    Badges4languages_Plugin
